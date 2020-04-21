6f5047a568dc0900a3e7b0544152a693ff6fe2ab
reshetnikd <dmitry.reshetnik@icloud.com>
2020-04-21 20:03:03 +0300

    Add UIImagePickerController for importing a picture

--- Instafilters/ViewController.swift
+++ Instafilters/ViewController.swift
@@ -8,14 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var intensity: UISlider!
+    var currentImage: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "YACIFP"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
     }
 
     @IBAction func changeFilter(_ sender: UIButton) {
@@ -25,6 +28,21 @@ class ViewController: UIViewController {
     }
     
     @IBAction func intensityChanged(_ sender: UISlider) {
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        dismiss(animated: true)
+        
+        currentImage = image
     }
     
 }
